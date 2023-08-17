c10fdd80765a60dd8b7954caf0620fa99a2707ec
Sheep124 <kennethwmy341@gmail.com>
2023-08-18 01:07:55 +0800

    entity
    added some attributes for each entty

--- src/main/java/entity/Course.java
+++ src/main/java/entity/Course.java
@@ -8,6 +8,30 @@ package entity;
  *
  * @author kenne
  */
-public class Course {
+public class Course {  
+    
+    
+    private String courseID;
+    private String name;
+
+    public String getCourseID() {
+        return courseID;
+    }
+
+    public void setCourseID(String courseID) {
+        this.courseID = courseID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    
+    
+    
     
 }
--- src/main/java/entity/Teaching.java
+++ src/main/java/entity/Teaching.java
@@ -9,5 +9,25 @@ package entity;
  * @author kenne
  */
 public class Teaching {
+
+    /*
+    Teaching Assignment Subsystem
+Assign tutor to course
+Assign tutorial groups to tutor
+Assign a tutor to a tutorial group
+Search courses under a tutor
+Search tutors for a course
+List all tutors for a course
+List courses under a tutor
+Filter tutors based on criteria
+Generate relevant reports
+
+     */
+    private String tutorID;
+    private String name;
+    private String tutorialID;
+    
+    
+    
     
 }
